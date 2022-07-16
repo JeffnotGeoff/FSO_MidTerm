5f2b6798aa8edb86be0902ade9e01afac7d73ee9
Jeffery Treichel <JATreichel@student.fullsail.edu>
2022-07-15 20:17:13 -0700

    Completed Build

--- Window.cpp
+++ Window.cpp
@@ -2,6 +2,18 @@
 
 Window::Window() : wxFrame(nullptr, wxID_ANY, "Quiz Time!", wxPoint(200, 200), wxSize(300, 600)) {
 
+	
+	if (questionNumber == 0 || questionNumber == 2) {
+		answer = 2;
+	}
+	else if (questionNumber == 1) {
+		answer = 4;
+	}
+	else {
+		answer = 3;
+	}
+	
+
 	buttons = new wxButton * [rows];
 
 	wxGridSizer* grid = new wxGridSizer(rows, columns, 0, 0);
@@ -9,11 +21,33 @@ Window::Window() : wxFrame(nullptr, wxID_ANY, "Quiz Time!", wxPoint(200, 200), w
 	for (int y = 0; y < rows; y++) {
 		buttons[y] = new wxButton(this, 10000 + y);
 
+		buttons[y]->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Window::OnButtonClicked, this);
 
 		grid->Add(buttons[y], 1, wxEXPAND | wxALL);
 
-		if (y == 0)
+		if (y == 0) {
 			buttons[y]->Enable(false);
+			buttons[y]->SetLabel(questions[questionNumber]);
+		}
+		else {
+			switch (questionNumber) {
+			case 0:
+				buttons[y]->SetLabel(answers0[y - 1]);
+				break;
+			case 1:
+				buttons[y]->SetLabel(answers1[y - 1]);
+				break;
+			case 2:
+				buttons[y]->SetLabel(answers2[y - 1]);
+				break;
+			case 3:
+				buttons[y]->SetLabel(answers3[y - 1]);
+				break;
+			default:
+				buttons[y]->SetLabel("This should not appear. If it does, should I be scared?");
+				break;
+			}
+		}
 	}
 
 	/*
@@ -30,4 +64,15 @@ Window::Window() : wxFrame(nullptr, wxID_ANY, "Quiz Time!", wxPoint(200, 200), w
 
 Window::~Window() {
 	delete[] buttons;
+}
+
+void Window::OnButtonClicked(wxCommandEvent& evt) {
+	int y = (evt.GetId() - 10000);
+
+	if (y == answer) {
+		wxMessageBox("Correct!!");
+	}
+	else {
+		wxMessageBox("Incorrect");
+	}
 }
--- Window.h
+++ Window.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <string>
+//#include <string>    //How do Strings break the program.... What?
 #include "wx/wx.h"
 
 class Window : public wxFrame
@@ -11,6 +11,18 @@ public:
 	int columns = 1;
 	int rows = 5;
 
+	int questionNumber = rand() % 4;
+
+	int answer = 0;
+
 	wxButton** buttons; 
+
+	const char* questions[4] = {"What is 2+2?", "Which color does not apply to RGB?", "How many fingers on a Human Hand?", "How many legs on a spider?"};
+	const char* answers0[4] = { "1", "2", "3", "4" };
+	const char* answers1[4] = { "Red", "Green", "Blue", "Yellow" };
+	const char* answers2[4] = { "4", "5", "6", "7" };
+	const char* answers3[4] = { "4", "6", "8", "10" };
+	
+	void OnButtonClicked(wxCommandEvent& evt);
 };
 
